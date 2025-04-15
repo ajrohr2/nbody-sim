6fcab106be2d2eb0b2cc27594e392b1e5a2e52e1
Aiden Rohrbach <arohrbachj@protonmail.com>
2025-04-15 19:02:07 -0400

    Correct definition of Earth

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,7 +6,7 @@ const SUITE = BenchmarkGroup()
 SUITE["simulate"] = BenchmarkGroup()
 
 for m in 1:1:5
-    bodies = [BasicNBodySim.Planet(5.9722e24, [0.0, 0.0], [0.0, 0.0]), :blue]
+    bodies = [BasicNBodySim.Planet(5.9722e24, [0.0, 0.0], [0.0, 0.0], :blue)]
     append!(bodies, [BasicNBodySim.Moon(7.348e22, (2.66e-6).*[-(3.84e8)*sin((2*pi*n)/m), (3.84e8)*cos((2*pi*n/m))], [(3.84e8)*cos((2*pi*n)/m), (3.84e8)*sin((2*pi*n/m))], :black) for n in 1:m])
 
     SUITE["simulate"][n] = @benchmarkable(BasicNBodySim.simulate_without_save(bs, 31536000.0, 365*24*6, 365),evals=1,samples=100,setup=(bs = $bodies))
