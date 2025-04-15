fba0a50b63c16d49571da828a76a830bacf1cb98
Aiden Rohrbach <arohrbachj@protonmail.com>
2025-04-15 19:00:31 -0400

    Update inclusion of package

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,13 +1,13 @@
 using BenchmarkTools
-using BasicNBodySim
+import BasicNBodySim
 
 const SUITE = BenchmarkGroup()
 
 SUITE["simulate"] = BenchmarkGroup()
 
 for m in 1:1:5
-    bodies = [Planet(5.9722e24, [0.0, 0.0], [0.0, 0.0]), :blue]
-    append!(bodies, [Moon(7.348e22, (2.66e-6).*[-(3.84e8)*sin((2*pi*n)/m), (3.84e8)*cos((2*pi*n/m))], [(3.84e8)*cos((2*pi*n)/m), (3.84e8)*sin((2*pi*n/m))], :black) for n in 1:m])
+    bodies = [BasicNBodySim.Planet(5.9722e24, [0.0, 0.0], [0.0, 0.0]), :blue]
+    append!(bodies, [BasicNBodySim.Moon(7.348e22, (2.66e-6).*[-(3.84e8)*sin((2*pi*n)/m), (3.84e8)*cos((2*pi*n/m))], [(3.84e8)*cos((2*pi*n)/m), (3.84e8)*sin((2*pi*n/m))], :black) for n in 1:m])
 
-    SUITE["simulate"][n] = @benchmarkable(simulate_without_save(bs, 31536000.0, 365*24*6, 365),evals=1,samples=100,setup=(bs = $bodies))
+    SUITE["simulate"][n] = @benchmarkable(BasicNBodySim.simulate_without_save(bs, 31536000.0, 365*24*6, 365),evals=1,samples=100,setup=(bs = $bodies))
 end
