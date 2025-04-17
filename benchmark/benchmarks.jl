c938313253457db77ff33d3beef75b00433aa749
Aiden Rohrbach <arohrbachj@protonmail.com>
2025-04-17 13:58:24 -0400

    Change benchmark keys to strings

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,5 +9,5 @@ for m in 1:1:5
     bodies = Vector{BasicNBodySim.Body}([BasicNBodySim.Planet(5.9722e24, [0.0, 0.0], [0.0, 0.0], :blue)])
     append!(bodies, [BasicNBodySim.Moon(7.348e22, (2.66e-6).*[-(3.84e8)*sin((2*pi*n)/m), (3.84e8)*cos((2*pi*n/m))], [(3.84e8)*cos((2*pi*n)/m), (3.84e8)*sin((2*pi*n/m))], :black) for n in 1:m])
 
-    SUITE["simulate"][m] = @benchmarkable(BasicNBodySim.simulate_without_save(bs, 31536000.0, 365*24*6),evals=1,samples=100,setup=(bs = $bodies))
+    SUITE["simulate"][string(m)] = @benchmarkable(BasicNBodySim.simulate_without_save(bs, 31536000.0, 365*24*6),evals=1,samples=100,setup=(bs = $bodies))
 end
