f5c46d1c519589c6b2ebebc00525db5e181bdb69
Aiden Rohrbach <arohrbachj@protonmail.com>
2025-04-17 11:54:22 -0400

    Added sleep line in update loop to test asv

--- src/BasicNBodySim.jl
+++ src/BasicNBodySim.jl
@@ -119,6 +119,7 @@ function simulate_without_save(all_bodies::Vector{Body}, time::Float64, steps::I
         for body in all_bodies
             update!(body, all_bodies, time / steps)
         end
+        sleep(1)
     end
 end
 
