a9eda173641e55e09b0b2782a0d4b2e490997326
Aiden Rohrbach <arohrbachj@protonmail.com>
2025-04-25 12:39:30 -0400

    Docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ using Documenter, BasicNBodySim
 
 makedocs(sitename="BasicNBodySim Documentation", pages = ["Home" => "index.md", "Examples" => "examples.md", "Body Constructors" => "constructors.md", "Simulation Functions" => "functions.md"]) 
 
-# deploydocs(repo = "github.com/ajrohr2/nbody-sim.git")
+deploydocs(repo = "github.com/ajrohr2/nbody-sim.git")
